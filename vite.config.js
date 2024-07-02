fd5bd36ebcf9a94b91abeb6dcd783dba329ed418
Arpit Malik <arpit08malik2000@gmail.com>
2024-07-02 21:03:22 +0530

    feat(main): added basic dev tools

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import scss from 'vite-plugin-scss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  plugins: [react(), scss()],
+  plugins: [react()],
   build: {
-    minify: true
-  }
-})
+    minify: true,
+  },
+});
