6456a968241c2f6835664406a56c6f6d12ae2e93
Arpit Malik <arpit08malik2000@gmail.com>
2024-07-02 15:29:00 +0530

    init commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import scss from 'vite-plugin-scss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+
+  plugins: [react(), scss()],
+  build: {
+    minify: true
+  }
+})
