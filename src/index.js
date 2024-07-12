88a3562d8eaecfd1ed4160d9d7b5d1a38a7b2cad
Arpit Malik <arpit08malik2000@gmail.com>
2024-07-13 01:13:55 +0530

    feat(main): added service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { ENVS } from './configs/app';
+import SWRegistration from './services/SWRegistration';
 
 import('./bootstrap').then(({ mount }) =>
   mount(document.getElementById('app')),
 );
+
+SWRegistration.register();
 
 if (process.env.NODE_ENV !== ENVS.PROD) {
   import('./utils/reportWebVitals').then(({ default: func }) => func());
