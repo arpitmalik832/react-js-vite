680719f9743603a3b1116d8e6a023ede4cd36a70
Arpit Malik <arpit08malik2000@gmail.com>
2024-10-09 00:20:35 +0530

    chore(main): updating scripts

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,11 +2,10 @@
  * This file is used to export the store based on the environment.
  * @file This file is saved as `redux/store.js`.
  */
-import { APP } from '@arpitmalik832/react-js-rollup-monorepo-library';
-
+import { ENVS } from '../../../build_utils/config/index.mjs';
 import devStore from './store.dev.mjs';
 import prodStore from './store.prod.mjs';
 
-const store = process.env.NODE_ENV === APP.ENVS.PROD ? prodStore : devStore;
+const store = process.env.APP_ENV === ENVS.PROD ? prodStore : devStore;
 
 export default store;
