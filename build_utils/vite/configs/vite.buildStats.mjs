35ae07ffb339e248a140337706cf3bf605987241
Arpit Malik <arpit.malik@paytmmoney.com>
2024-10-18 15:30:08 +0530

    chore(main): upgrading dependencies

--- build_utils/vite/configs/vite.buildStats.mjs
+++ build_utils/vite/configs/vite.buildStats.mjs
@@ -5,10 +5,7 @@
 import buildStats from '../customPlugins/buildStatsPlugin.mjs';
 
 const timestamp = new Date().toISOString().replace(/:/g, '-');
-const path =
-  process.env.IS_STORYBOOK === 'true'
-    ? `distInfo/storybook/${process.env.APP_ENV}/buildStats`
-    : `distInfo/${process.env.APP_ENV}/buildStats`;
+const path = `distInfo/${process.env.IS_STORYBOOK === 'true' ? 'storybook' : 'main'}/${process.env.APP_ENV}/buildStats`;
 
 const config = {
   plugins: [buildStats(`${path}/${timestamp}.json`)],
